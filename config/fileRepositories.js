ca4b92c116281c5594c06c8a5ca153e300f1caa4
Srinath Janakiraman <vjsrinath@live.com>
2015-09-17 02:54:05 +0530

    removed hard coded db credentials and made changes to read credentials from environment variables

--- config/fileRepositories.js
+++ config/fileRepositories.js
@@ -1,11 +1,12 @@
 /**
  * Created by developer on 5/31/15.
  */
+console.error("process.env.GRID_FS_URL: " + process.env.GRID_FS_URL);
 module.exports = {
   fileRepositories: {
 
     gridfs: {
-      url: 'mongodb://127.0.0.1:27017/ludicrum',
+      url: process.env.GRID_FS_URL || 'mongodb://127.0.0.1:27017/ludicrum',
       root: 'files'
     }
   }
